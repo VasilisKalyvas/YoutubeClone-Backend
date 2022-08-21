c57299baa19ade624648efb66312214a033bd38c
Βασιλης Καλυβας <bkalivas8@gmail.com>
2022-08-21 16:37:34 +0300

    make it better

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
     cors({
-      origin: ["http://localhost:3000"],
+      origin: ["https://mern-clonetube-app.herokuapp.com/"],
       methods: ["GET", "POST","PUT", "DELETE"],
       credentials: true,
     })
